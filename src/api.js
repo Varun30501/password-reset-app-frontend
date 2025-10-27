307d5ffea57edd80dac21239b9dfcf2bfb4a6db6
Varun30501 <vatsanvenky@gmail.com>
2025-10-27 15:08:28 +0530

    new commit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://password-reset-app-backend-xqyw.onrender.com/api/auth", // backend link
+  baseURL: "https://password-reset-app-backend-x6oo.onrender.com/api/auth", // backend link
 });
 
 export default API;
